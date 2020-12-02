e0ba4a6fbea36143b91a009b2633dd256b339ac7
Sandra <alecsandra.serediuc@gmail.com>
2020-12-02 10:29:35 +0100

    Draft fetching data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, 
+  { useEffect, useState } from 'react';
 import './App.css';
+import AppContext from './Context/Provider';
+import { Context } from './Context/Provider';
 
-function App() {
+
+const App = () => {
+  const { state, addPhonesList } = React.useContext(Context);
+  // const [ list, setList ] = useState([]);
+  const [ mounted, setMounted ] = useState(false)
+
+  useEffect(() => {
+    fetch('http://localhost:8000/phones')
+      .then(res => res.json())
+      .then(data =>{
+                      addPhonesList(data)
+                      setMounted(true)
+                    })
+  // console.log(state.phones)
+
+  }, [])
+  // console.log(state.phones)
+
   return (
+    <AppContext>
+    
     <div className="App">
-     App
+    {mounted === false ? 'hello' :
+     'App' }
     </div>
+ </AppContext>
   );
 }
 
