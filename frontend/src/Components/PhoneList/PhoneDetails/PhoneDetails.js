5327bb02ae00facd38da91a76c4b528db86e0c57
Sandra <alecsandra.serediuc@gmail.com>
2020-12-03 14:23:30 +0100

    Fetrching id route and displaying more details

--- frontend/src/Components/PhoneList/PhoneDetails/PhoneDetails.js
+++ frontend/src/Components/PhoneList/PhoneDetails/PhoneDetails.js
@@ -1,15 +1,47 @@
 import React, 
   { useEffect, useState } from 'react';
+import { Context } from '../../../Context/Provider';
+import { useParams } from 'react-router-dom'
+
+const PhoneList = () => {
+  const { state, addPhonesList } = React.useContext(Context);
+  // const [ mounted, setMounted ] = useState(false)
+  const [ phone, setPhone]= useState({})
+  const { id } = useParams();
+  
+  useEffect(() => {
+    fetch(`http://localhost:8000/phones/${id}`)
+      .then(res => res.json())
+      .then(data =>{
+        setPhone(data)
+        // setMounted(true)
+      })
+  //     // .catch(err=>{console.log(err)})
+
+  },[])
+console.log(phone)
+console.log(phone.name)
 
 
-const PhoneDetails = () => {
 
-  return (
-   <div> 
-    PhoneDetails
-   </div>
 
+  return (  
+    <div className="list">
+       <div>
+        <p>{id}</p>
+         <p>{phone.name}</p>
+        <p>{phone.manufacturer}</p>
+        <p>{phone.description}</p>
+        <p>{phone.color}</p>
+        <p>{phone.price}</p>
+        <p>{phone.screen}</p>
+        <p>{phone.processor}</p>
+        <p>{phone.ram}</p> 
+        {/* <p>{item.imageFileName}</p> */}
+        </div>
+
+    </div>
   );
 }
 
-export default PhoneDetails;
+export default PhoneList;
