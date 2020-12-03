4a1abd3f18defeb9540df200c92db4a711e78844
Sandra <alecsandra.serediuc@gmail.com>
2020-12-03 20:42:54 +0100

    added scroll to top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,37 +4,38 @@ import './App.css';
 import AppContext from './Context/Provider';
 import Home from './Components/Home/Home';
 import PhoneDetails from './Components/PhoneList/PhoneDetails/PhoneDetails';
-import Navbar from './Components/Navbar/Navbar'
+import Navbar from './Components/Navbar/Navbar';
+import ScrollToTop from '../src/Utils/ScrollToTop';
 
 
 const App = () => {
 
   return (
     <AppContext>
-
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={(props) => (
-          <div>
-            <Navbar text={"Home"}/>
-            <Home {...props} />
-          </div>
-          )}
-        />
-        <Route
-          exact
-          path="/phones/:id"
-          render={(props) => (
-          <div>
-            <Navbar text={"Back Home"}/>
-            <PhoneDetails {...props} />
-          </div>
-          )}
-        />
-      </Switch> 
-
+      <ScrollToTop>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+            <div>
+              <Navbar text={"Home"}/>
+              <Home {...props} />
+            </div>
+            )}
+          />
+          <Route
+            exact
+            path="/phones/:id"
+            render={(props) => (
+            <div>
+              <Navbar text={"Back Home"}/>
+              <PhoneDetails {...props} />
+            </div>
+            )}
+          />
+        </Switch> 
+      </ScrollToTop>
     </AppContext>
   );
 }
