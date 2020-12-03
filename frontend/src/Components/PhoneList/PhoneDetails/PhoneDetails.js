b63148f1cf02d2efb31e606c6de0e5db4dd6969a
Sandra <alecsandra.serediuc@gmail.com>
2020-12-03 21:30:52 +0100

    fix nav

--- frontend/src/Components/PhoneList/PhoneDetails/PhoneDetails.js
+++ frontend/src/Components/PhoneList/PhoneDetails/PhoneDetails.js
@@ -7,53 +7,50 @@ import PhotoCar from './PhotosCar'
 
 const PhoneList = () => {
   const { state, addPhonesList } = React.useContext(Context);
-  // const [ mounted, setMounted ] = useState(false)
   const [ phone, setPhone]= useState({})
   const { id } = useParams();
+  const [error, setError] = useState(false)
   
   useEffect(() => {
     fetch(`http://localhost:8000/phones/${id}`)
       .then(res => res.json())
       .then(data =>{
         setPhone(data)
-        // setMounted(true)
       })
       .catch(err=>{
+        setError(true)
         console.log(err,'error')
         alert('Could not find any phone with this id!')
       })
 
   },[])
-console.log(phone)
-console.log(phone.name)
-
-
-
+// console.log(phone)
+// console.log(phone.name)
 
   return (  
-    <div>
-     {id &&
-     <div className="list-card">
-      <div>
-          <img className='img-card'
-          src='https://images.unsplash.com/photo-1537589376225-5405c60a5bd8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=80' />
+    <div className={error ? 'none':'details'}>
+      {id &&
+        <div className="list-card">
+          <div>
+              {/* <p>{item.imageFileName}</p> */}
+              <img className='img-card'
+              src='https://images.unsplash.com/photo-1537589376225-5405c60a5bd8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=80' />
           </div>
-       <div>
-        <p className='title'>{phone.name}</p>
-        <p className='price'>{phone.price}$</p>
-        <p className='description'>{phone.description}</p>
-        <div className='details-added'>
-        <p><span className='details-text'>Manufacturer:</span> {phone.manufacturer}</p>
-        <p><span className='details-text'>Color:</span> {phone.color}</p>
-        <p><span className='details-text'>Screen:</span> {phone.screen}</p>
-        <p><span className='details-text'>Processor:</span> {phone.processor}</p>
-        <p><span className='details-text'>Ram:</span> {phone.ram}</p> 
-        </div>
-        {/* <p>{item.imageFileName}</p> */}
-      </div>
+          <div>
+            <p className='title'>{phone.name}</p>
+            <p className='price'>{phone.price}$</p>
+            <p className='description'>{phone.description}</p>
+          <div className='details-added'>
+            <p><span className='details-text'>Manufacturer:</span> {phone.manufacturer}</p>
+            <p><span className='details-text'>Color:</span> {phone.color}</p>
+            <p><span className='details-text'>Screen:</span> {phone.screen}</p>
+            <p><span className='details-text'>Processor:</span> {phone.processor}</p>
+            <p><span className='details-text'>Ram:</span> {phone.ram}</p> 
+          </div>
+          </div>
         </div>
      }
-     <PhotoCar />
+          <PhotoCar />
     </div>
   );
 }
