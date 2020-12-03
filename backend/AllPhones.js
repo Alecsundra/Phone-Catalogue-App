3539eea63a7f30fad6a00e4b3507699bf5003baf
Sandra <alecsandra.serediuc@gmail.com>
2020-12-03 16:40:20 +0100

    Complete file with more info

--- backend/AllPhones.js
+++ backend/AllPhones.js
@@ -1,40 +1,80 @@
+
+
 const AllPhones = [
+
     {
-    "id": 0,
-    "name": "iPhone 7",
-    "manufacturer": "Apple",
-    "description": "lorem ipsum dolor sit amet consectetur.",
-    "color": "black",
-    "price": 769,
-    "imageFileName": "IPhone_7.png",
-    "screen": "4,7 inch IPS",
-    "processor": "A10 Fusion",
-    "ram": 2
-    },
-    {
-        "id": 1,
+        "id": 0,
         "name": "iPhone 7",
         "manufacturer": "Apple",
-        "description": "lorem ipsum dolor sit amet consectetur.",
+        "description": "Quisque pellentesque sed diam sed semper. Curabitur aliquet ante eu metus congue, nec faucibus lacus faucibus. Nunc nec tortor id felis malesuada iaculis in ut lacus.",
         "color": "black",
         "price": 769,
-        "imageFileName": "IPhone_7.png",
+        "imageFileName":"../../Utils/img/iphone7.jpg",
         "screen": "4,7 inch IPS",
         "processor": "A10 Fusion",
         "ram": 2
-        },
+    },
     {
-            "id": 2,
-            "name": "iPhone 7",
-            "manufacturer": "Apple",
-            "description": "lorem ipsum dolor sit amet consectetur.",
-            "color": "black",
-            "price": 769,
-            "imageFileName": "IPhone_7.png",
-            "screen": "4,7 inch IPS",
-            "processor": "A10 Fusion",
-            "ram": 2
-            }   
-    ]
+        "id": 1,
+        "name": "iPhone 8",
+        "manufacturer": "Apple",
+        "description": "Quisque pellentesque sed diam sed semper. Curabitur aliquet ante eu metus congue, nec faucibus lacus faucibus. Nunc nec tortor id felis malesuada iaculis in ut lacus.",
+        "color": "white",
+        "price": 869,
+        "imageFileName": "../../Utils/img/iphone8.jpg",
+        "screen": "4,6 inch IPS",
+        "processor": "A11 Fusion",
+        "ram": 3
+    },
+    {
+        "id": 2,
+        "name": "iPhone X ",
+        "manufacturer": "Apple",
+        "description": "Quisque pellentesque sed diam sed semper. Curabitur aliquet ante eu metus congue, nec faucibus lacus faucibus. Nunc nec tortor id felis malesuada iaculis in ut lacus.",
+        "color": "pink",
+        "price": 969,
+        "imageFileName": "../../Utils/img/iphone10.jpg",
+        "screen": "4,8 inch IPS",
+        "processor": "A10 Fusion",
+        "ram": 3
+    },
+    {
+        "id": 3,
+        "name": "iPhone XR ",
+        "manufacturer": "Apple",
+        "description": "Quisque pellentesque sed diam sed semper. Curabitur aliquet ante eu metus congue, nec faucibus lacus faucibus. Nunc nec tortor id felis malesuada iaculis in ut lacus.",
+        "color": "blak",
+        "price": 899,
+        "imageFileName": "../../Utils/img/iphoneXR.jpg",
+        "screen": "4,9 inch IPS",
+        "processor": "A10 Fusion",
+        "ram": 2
+    },
+    {
+        "id": 4,
+        "name": "iPhone 8 Plus ",
+        "manufacturer": "Apple",
+        "description": "Quisque pellentesque sed diam sed semper. Curabitur aliquet ante eu metus congue, nec faucibus lacus faucibus. Nunc nec tortor id felis malesuada iaculis in ut lacus.",
+        "color": "white",
+        "price": 769,
+        "imageFileName": "../../Utils/img/iphone8plus.jpg",
+        "screen": "4,6 inch IPS",
+        "processor": "A10 Fusion",
+        "ram": 2
+    },
+    {
+        "id": 5,
+        "name": "iPhone 11 Pro",
+        "manufacturer": "Apple",
+        "description": "Quisque pellentesque sed diam sed semper. Curabitur aliquet ante eu metus congue, nec faucibus lacus faucibus. Nunc nec tortor id felis malesuada iaculis in ut lacus.",
+        "color": "grey",
+        "price": 1169,
+        "imageFileName": "../../Utils/img/iphone11pro.jpg",
+        "screen": "5 inch IPS",
+        "processor": "A10 Fusion",
+        "ram": 3
+    }     
+    
+]
 
 module.exports = AllPhones;
